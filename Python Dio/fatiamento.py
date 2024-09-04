cd8d739e5f4267dfcf7cc8c4301f41945ec7c423
Michel <michelgbo@gmail.com>
2024-09-04 16:46:40 -0300

    stringss

---
+++ Python Dio/fatiamento.py
@@ -0,0 +1,13 @@
+nome = "Michel Guedes brandao"
+
+print(nome[0])
+
+print(nome[:6])
+
+print(nome[7:])
+
+print(nome[7:13:2])
+
+print(nome[:])
+
+print(nome[::-1])
---
+++ Python Dio/stringmultipas.py
@@ -0,0 +1,20 @@
+nome = 'Michel'
+
+mensagem = f'''Ola {nome}
+              bem vindo
+              Ate logo'''
+
+print(mensagem)
+
+msg = """
+    ======== MENU ========
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+    
+    = 
+    
+    Obrigado !!!
+    """
+
+print(msg)
