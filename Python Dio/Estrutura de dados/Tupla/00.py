d28bb9cb5e5178a7d6949669373238ff58266ce2
Michel <michelgbo@gmail.com>
2024-09-18 15:54:25 -0300

    tupla

---
+++ Python Dio/Estrutura de dados/Tupla/00.py
@@ -0,0 +1,11 @@
+frutas = ("laranja", "maca", "uva",)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
---
+++ Python Dio/Estrutura de dados/Tupla/01.py
@@ -0,0 +1,7 @@
+frutas = ("maca", "pera", "laranja", 'uva')
+
+print(frutas[0])
+
+print(frutas[2])
+
+print(frutas[-1])
