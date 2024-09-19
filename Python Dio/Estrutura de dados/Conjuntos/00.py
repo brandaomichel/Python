2581ad2e3e651a00efac49bf2114383f07600fe0
Michel <michelgbo@gmail.com>
2024-09-19 15:21:21 -0300

    set

---
+++ Python Dio/Estrutura de dados/Conjuntos/00.py
@@ -0,0 +1,8 @@
+print(set([1, 2, 3, 1, 3, 4]))
+
+print(set("abacaxi"))
+
+print(set(("palio", "gol", "celta", "palio")))
+
+ling = {"python", "java", "python"}
+print(ling)
---
+++ Python Dio/Estrutura de dados/Conjuntos/07.py
@@ -0,0 +1,9 @@
+conjunto_a = {1, 2, 3}
+conjunto_b = {4, 1, 2, 5, 6, 3}
+
+print(conjunto_a.issubset(conjunto_b))
+print(conjunto_b.issubset(conjunto_a))
+
+
+print(conjunto_a.issuperset(conjunto_b))
+print(conjunto_b.issuperset(conjunto_a))
